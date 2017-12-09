db98d01dbd3ce1f0d00bc3d708284fd2af00e751
U-node\Kreso <kivkovic@gmail.com>
2017-12-09 21:17:37 +0100

    Fixed whitespace

--- index.js
+++ index.js
@@ -2,244 +2,244 @@
 
 class Expression {
 
-	constructor(string) {
-    	this.string = string;
-    	this._escaped = null;
-    	this._parentheses = null;
-    	this._brackets = null;
-	}
+    constructor(string) {
+        this.string = string;
+        this._escaped = null;
+        this._parentheses = null;
+        this._brackets = null;
+    }
 
-	get escaped () {
-		if (this._escaped) {
-			return this._escaped;
-		}
+    get escaped () {
+        if (this._escaped) {
+            return this._escaped;
+        }
 
-		this._escaped = {};
+        this._escaped = {};
 
-		for (let i = 0, willEscape = false, quoted = false; i < this.string.length; i++) {
-    		if (this.string[i] === '\\' && !willEscape) {
-    			willEscape = true;
+        for (let i = 0, willEscape = false, quoted = false; i < this.string.length; i++) {
+            if (this.string[i] === '\\' && !willEscape) {
+                willEscape = true;
 
-    		} else if (willEscape) {
-    			willEscape = false;
-    			this._escaped[i] = true;
-    		}
-    	}
+            } else if (willEscape) {
+                willEscape = false;
+                this._escaped[i] = true;
+            }
+        }
 
-    	return this._escaped;
-	}
+        return this._escaped;
+    }
 
-	get parentheses () {
-		if (!this._parentheses) {
-			this._parentheses = this.findBracketed({left: '(', right: ')', oneLevel: false}) || [];
-		}
+    get parentheses () {
+        if (!this._parentheses) {
+            this._parentheses = this.findBracketed({left: '(', right: ')', oneLevel: false}) || [];
+        }
 
-		return this._parentheses;
-	}
+        return this._parentheses;
+    }
 
-	get brackets () {
-		if (!this._brackets) {
-			this._brackets = this.findBracketed({left: '[', right: ']', oneLevel: true}) || [];
-		}
+    get brackets () {
+        if (!this._brackets) {
+            this._brackets = this.findBracketed({left: '[', right: ']', oneLevel: true}) || [];
+        }
 
-		return this._brackets;
-	}
+        return this._brackets;
+    }
 
-	findBracketed({left = '(', right = ')', oneLevel = false}) {
+    findBracketed({left = '(', right = ')', oneLevel = false}) {
 
-		const open = [], stack = [];
+        const open = [], stack = [];
 
-		for (let i = 0; i < this.string.length; i++) {
-			if (this.string[i] === left && !this.escaped[i]) {
-				open.push(new Expression.Range({from: i}));
+        for (let i = 0; i < this.string.length; i++) {
+            if (this.string[i] === left && !this.escaped[i]) {
+                open.push(new Expression.Range({from: i}));
 
-			} else if (this.string[i] === right && !this.escaped[i]) {
-				open[open.length-1].to = i + 1;
-				stack.push(open.pop());
-			}
-		}
+            } else if (this.string[i] === right && !this.escaped[i]) {
+                open[open.length-1].to = i + 1;
+                stack.push(open.pop());
+            }
+        }
 
-		stack.sort((a, b) => a.from - b.from);
+        stack.sort((a, b) => a.from - b.from);
 
-		return stack.length
-			? oneLevel
-				? stack.filter(r => stack.filter(p => p.contains(r)).length === 1)
-				: stack
-			: false;
-	}
+        return stack.length
+            ? oneLevel
+                ? stack.filter(r => stack.filter(p => p.contains(r)).length === 1)
+                : stack
+            : false;
+    }
 
     findTokens(token, filter, startOffset = 0) {
 
         const tokens = [];
 
-		 if (token instanceof RegExp) {
-        	let match;
-    		while ((match = token.exec(this.string)) !== null) {
-	        	if (!this.escaped[match.index]) {
-	        		const range = new Expression.Range({from: match.index, to: match.index + match[0].length});
-	        		if (filter(range)) {
-			        	tokens.push(range);
-			        }
-		        }
-	        }
+         if (token instanceof RegExp) {
+            let match;
+            while ((match = token.exec(this.string)) !== null) {
+                if (!this.escaped[match.index]) {
+                    const range = new Expression.Range({from: match.index, to: match.index + match[0].length});
+                    if (filter(range)) {
+                        tokens.push(range);
+                    }
+                }
+            }
 
-    	 } else {
+         } else {
 
-	 	    let index, offset = startOffset;
-		    while ((index = this.string.indexOf(token, offset)) != -1){
-		    	offset = index + token.length;
-        		const range = new Expression.Range({from: index, to: index + token.length});
-        		if (filter(range)) {
-		        	tokens.push(range);
-		        }
-		    }
-    	 }
+             let index, offset = startOffset;
+            while ((index = this.string.indexOf(token, offset)) != -1){
+                offset = index + token.length;
+                const range = new Expression.Range({from: index, to: index + token.length});
+                if (filter(range)) {
+                    tokens.push(range);
+                }
+            }
+         }
 
         return tokens;
     }
 
     replace(range_s, string) {
-    	const constructed = [], ranges = range_s.constructor === Array ? range_s : [range_s];
-    	let last = 0;
+        const constructed = [], ranges = range_s.constructor === Array ? range_s : [range_s];
+        let last = 0;
 
-    	for (let i = 0; i < ranges.length; i++) {
-    		constructed.push(this.string.slice(last, ranges[i].from));
-    		constructed.push(string);
-    		last = ranges[i].to;
-    	}
+        for (let i = 0; i < ranges.length; i++) {
+            constructed.push(this.string.slice(last, ranges[i].from));
+            constructed.push(string);
+            last = ranges[i].to;
+        }
 
-    	constructed.push(this.string.slice(last));
+        constructed.push(this.string.slice(last));
 
-    	return new Expression(constructed.join(''))
+        return new Expression(constructed.join(''))
     }
 }
 
 Expression.Range = class {
-	constructor({from = -1, to = -1}) {
-		this.from = from;
-		this.to = to;
-	}
+    constructor({from = -1, to = -1}) {
+        this.from = from;
+        this.to = to;
+    }
 
-	contains(range) {
-		return (typeof range.from !== 'undefined' && typeof range.to !== 'undefined')
-			? this.from <= range.from && this.to >= range.to
-			: this.from <= range && this.to >= range;
-	}
+    contains(range) {
+        return (typeof range.from !== 'undefined' && typeof range.to !== 'undefined')
+            ? this.from <= range.from && this.to >= range.to
+            : this.from <= range && this.to >= range;
+    }
 
-	isBracketed(brackets) {
+    isBracketed(brackets) {
 
-		for (let i = 0; i < brackets.length; i++) {
-			if (brackets[i].contains(this)) {
-				return true;
-			}
-		}
-		return false;
-	}
+        for (let i = 0; i < brackets.length; i++) {
+            if (brackets[i].contains(this)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
 
 class ReRegex {
-	constructor(string, flags = 'g') {
-		this._string = string;
-		this.flags = flags;
+    constructor(string, flags = 'g') {
+        this._string = string;
+        this.flags = flags;
 
-		this.expression = new Expression(string);
-		this.brackets = this.expression.brackets;
+        this.expression = new Expression(string);
+        this.brackets = this.expression.brackets;
 
-		this._locked = true;
-	}
+        this._locked = true;
+    }
 
-	get string () {
-		return this._string;
-	}
+    get string () {
+        return this._string;
+    }
 
-	set string (value) {
-		if (this._locked) {
-    		throw new ReferenceError(`Could not set read-only value`);
-    	}
-    	this._string = value;
-	}
+    set string (value) {
+        if (this._locked) {
+            throw new ReferenceError(`Could not set read-only value`);
+        }
+        this._string = value;
+    }
 
-	findGroups() {
-		if (this.groups) return this.groups;
+    findGroups() {
+        if (this.groups) return this.groups;
 
-		const
-			notBracketed = t => !t.isBracketed(this.brackets),
-			notSpecial = t => this.expression.string[t.from + 1] !== '?';
+        const
+            notBracketed = t => !t.isBracketed(this.brackets),
+            notSpecial = t => this.expression.string[t.from + 1] !== '?';
 
-		this.groups = this.expression.parentheses
-			.filter(notBracketed)
-			.filter(notSpecial);
+        this.groups = this.expression.parentheses
+            .filter(notBracketed)
+            .filter(notSpecial);
 
-		if (this.groups[0].from !== 0 || this.groups[0].to != this.expression.string.length) {
-			this.groups.unshift(new Expression.Range({from: 0, to: this.expression.string.length}));
-		}
+        if (this.groups[0].from !== 0 || this.groups[0].to != this.expression.string.length) {
+            this.groups.unshift(new Expression.Range({from: 0, to: this.expression.string.length}));
+        }
 
-		return this.groups;
-	}
+        return this.groups;
+    }
 
-	noncapturing() {
+    noncapturing() {
 
-		ReRegex.noncapturingCache = ReRegex.noncapturingCache || {};
-		if (ReRegex.noncapturingCache[this.string]) {
-			return new ReRegex(ReRegex.noncapturingCache[this.string]);
-		}
+        ReRegex.noncapturingCache = ReRegex.noncapturingCache || {};
+        if (ReRegex.noncapturingCache[this.string]) {
+            return new ReRegex(ReRegex.noncapturingCache[this.string]);
+        }
 
-		this.groups = this.findGroups();
-		let replaced = this.string, offset = 0;
+        this.groups = this.findGroups();
+        let replaced = this.string, offset = 0;
 
-		for (let i = 0; i < this.groups.length; i++) {
-			if (replaced[this.groups[i].from + offset] === '(') {
-				replaced = replaced.slice(0, this.groups[i].from + offset + 1) + '?:' + replaced.slice(this.groups[i].from + offset + 1);
-				offset += 2;
-			}
-		}
+        for (let i = 0; i < this.groups.length; i++) {
+            if (replaced[this.groups[i].from + offset] === '(') {
+                replaced = replaced.slice(0, this.groups[i].from + offset + 1) + '?:' + replaced.slice(this.groups[i].from + offset + 1);
+                offset += 2;
+            }
+        }
 
-		ReRegex.noncapturingCache[this.string] = replaced;
-		return new ReRegex(replaced);
-	}
+        ReRegex.noncapturingCache[this.string] = replaced;
+        return new ReRegex(replaced);
+    }
 
-	derecurse(n = 100) {
-		if (this.derecursed == n) {
-			return this;
-		}
+    derecurse(n = 100) {
+        if (this.derecursed == n) {
+            return this;
+        }
 
-		const
-			notBracketed = t => !t.isBracketed(this.brackets),
-			groups = this.findGroups();
+        const
+            notBracketed = t => !t.isBracketed(this.brackets),
+            groups = this.findGroups();
 
-		let current = this.expression;
+        let current = this.expression;
 
-		for (let i = 0; i <= n; i++) {
-			for (let j = 0; j < groups.length; j++) {
-				let replacement;
-				if (i < n) {
-					replacement = `(?:${new ReRegex(this.expression.string.slice(groups[j].from, groups[j].to)).noncapturing().string})`;
-				} else {
-					replacement = 'R^' // this is a guaranteed non-match, regardless of flags
-				}
-				current = current.replace(current.findTokens(`(?${j})`, notBracketed), replacement)
-			}
-		}
+        for (let i = 0; i <= n; i++) {
+            for (let j = 0; j < groups.length; j++) {
+                let replacement;
+                if (i < n) {
+                    replacement = `(?:${new ReRegex(this.expression.string.slice(groups[j].from, groups[j].to)).noncapturing().string})`;
+                } else {
+                    replacement = 'R^' // this is a guaranteed non-match, regardless of flags
+                }
+                current = current.replace(current.findTokens(`(?${j})`, notBracketed), replacement)
+            }
+        }
 
-		const regex = new ReRegex(current.string);
-		regex.derecursed = n;
-		return regex;
-	}
+        const regex = new ReRegex(current.string);
+        regex.derecursed = n;
+        return regex;
+    }
 
-	toRegExp(flags = 'g', n = 100) {
-		if (flags && !`${flags}`.match(/[gimuy]+/)) {
-			throw Error(`Invalid value '${flags}' given for parameter 'flags', must be a string with characters: 'g', 'i', 'm', 'u' or 'y'`);
-		}
+    toRegExp(flags = 'g', n = 100) {
+        if (flags && !`${flags}`.match(/[gimuy]+/)) {
+            throw Error(`Invalid value '${flags}' given for parameter 'flags', must be a string with characters: 'g', 'i', 'm', 'u' or 'y'`);
+        }
 
-		if ((n|0) != n || n < 0) {
-			throw Error(`Invalid value '${n}' given for parameter 'n', must be a positive integer`);
-		}
+        if ((n|0) != n || n < 0) {
+            throw Error(`Invalid value '${n}' given for parameter 'n', must be a positive integer`);
+        }
 
-		if (this.derecursed != n) {
-			return this.derecurse(n).toRegExp(flags, n);
-		}
-		return new RegExp(this.string, flags);
-	}
+        if (this.derecursed != n) {
+            return this.derecurse(n).toRegExp(flags, n);
+        }
+        return new RegExp(this.string, flags);
+    }
 }
 
 exports.ReRegex = ReRegex;
